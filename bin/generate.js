85b5f9110c1226732462a30d7f36c331ebccd26b
Kevin Ho <ho.kevins@gmail.com>
2018-05-08 12:51:34 -0400

    change generate and fix .gitignore rename

--- bin/generate.js
+++ bin/generate.js
@@ -2,6 +2,7 @@ var bluebird = require('bluebird');
 var path = require('path');
 var chalk = require('chalk');
 var ncp = bluebird.promisify(require('ncp').ncp);
+var rename = bluebird.promisify(require('fs').rename);
 
 ncp.limit = 16;
 
@@ -22,8 +23,14 @@ var copyFiles = function () {
     return ncp(generatorFilesPath, newProjectDir);
 };
 
+var renameGitignore = function () {
+    var oldPath = path.join(newProjectDir, 'gitignore.txt');
+    var newPath = path.join(newProjectDir, '.gitignore');
+    return rename(oldPath, newPath);
+};
+
 console.log(chalk.green('Generating your new, poppin\' fresh application!'));
-copyFiles().then(function () {
+copyFiles().then(renameGitignore).then(function () {
     console.log(chalk.blue('All done!'));
     console.log(chalk.red('Do not forget to'), chalk.yellow('npm install'), 'AND', chalk.green('gulp build'));
 });
